6650d7170a2d99f5f9c4a9783a374001cabdc0ae
Sherzod <shermanjones99@gmail.com>
2021-04-18 12:13:58 -0400

    expanded confirmation page logic

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -3,18 +3,54 @@ import Axios from "axios";
 import { API_ROOT_URL } from "../constants";
 
 class ConfirmationPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      confirmationSuccessful: false,
+      confirmationMessage: "",
+      desc: "",
+    };
+  }
   componentDidMount() {
     Axios.get(
       `${API_ROOT_URL}/confirm_email_token/${this.props.match.params.token}`
-    ).then((res) => {
-      console.log(res.data);
-    });
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          this.setState({
+            confirmationSuccessful: true,
+            confirmationMessage: "Your email has been confirmed!",
+            desc: "You should now be able to receive reminders!",
+          });
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          this.setState({
+            confirmationSuccessful: true,
+            confirmationMessage: "Your email is already confirmed",
+            desc: "You should already be able to receive reminders.",
+          });
+        } else {
+          this.setState({
+            confirmationSuccessful: false,
+            confirmationMessage: "Your email cannot be confirmed.",
+            desc: "Please make sure you have a valid confirmation link.",
+          });
+        }
+      });
   }
   render() {
+    let textColor = "#fff";
+    if (this.state.confirmationSuccessful) {
+      textColor = "#058807";
+    } else {
+      textColor = "#ea4242";
+    }
     return (
-      <div>
-        <h1>Your email has been confirmed.</h1>
-        <h3>You should now be able to receive emails!</h3>
+      <div style={{ color: textColor }}>
+        <h1>{this.state.confirmationMessage}</h1>
+        <h3>{this.state.desc}</h3>
       </div>
     );
   }
