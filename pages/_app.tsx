9f1796c1a1662aaabc30701f5844f571ffc14fab
Sherzod <shermanjones99@gmail.com>
2022-02-03 10:58:18 -0500

    add csrf once login happens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import { RouteGuard } from "../components/RouteGuard";
 import Axios from "axios";
-import Cookies from "js-cookie";
 
 import type { AppProps } from "next/app";
 import { UserProvider } from "../context/user.context";
@@ -10,8 +9,6 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.withCredentials = true;
-  Axios.defaults.headers.common["X-CSRF-TOKEN"] =
-    Cookies.get("csrf_access_token");
 
   return (
     <>
